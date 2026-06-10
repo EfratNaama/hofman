7e7f482d423825b98a0216ee791b7c73ded9b2ec
Tehila <5tehilacohen5@gmail.com>
2026-06-10 22:59:51 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>Welcome Efrat!</h1>
+        <h1>Welcome Hofman!</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
